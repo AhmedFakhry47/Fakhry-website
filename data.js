873e6856fa7b5c946bb132e60a7497c47410a21c
Ahmed Fakhry <44531149+AhmedFakhry47@users.noreply.github.com>
2024-07-25 02:03:46 +0900

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const projectsdata = [{
     "id": 1,
     "title": "Digitization of Handwritten Documents",
     "body": "DEEP LEARNING ‐ CLOSED SOURCE",
-    "image": "https://raw.githubusercontent.com/AhmedFakhry47/Fakhry-website/main/public/assets/ocr2.png",
+    "image": "https://raw.githubusercontent.com/AhmedFakhry47/Fakhry-website/main/public/assets/ocr4.png",
     // "image": "https://static.vecteezy.com/system/resources/previews/002/551/051/original/eye-cyber-circuit-future-technology-concept-background-free-vector.jpg",
     "pdf": "",
     "code": "",
