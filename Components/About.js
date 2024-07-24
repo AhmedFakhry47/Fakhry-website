42051d26be44b36e78d5866ae7208405de7ebef4
Ahmed Fakhry <44531149+AhmedFakhry47@users.noreply.github.com>
2024-07-24 21:21:43 +0900

    Update About.js

--- Components/About.js
+++ Components/About.js
@@ -37,7 +37,7 @@ function About() {
                                     alt="Picture of the author"
                                     // width={200}
                                 // height={300}
-                                    objectFit= "contain"
+                                    objectFit= "scale-down"
                                     objectPosition= "center"
                                 />
                             </Box >
