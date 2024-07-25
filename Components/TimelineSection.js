3766e98f0f59ab80231c5fc7d3a15f67da34744c
Ahmed Fakhry <44531149+AhmedFakhry47@users.noreply.github.com>
2024-07-25 17:39:06 +0900

    Update TimelineSection.js

--- Components/TimelineSection.js
+++ Components/TimelineSection.js
@@ -43,8 +43,6 @@ function TimelineSection() {
                                 <Image
                                     src={knuphoto}
                                     alt="Picture of the author"
-                                    width = {135}
-                                    height = {135}
                                 />
                             </TimelineDot>
                             <TimelineConnector sx={{ height: "100px" }} />
